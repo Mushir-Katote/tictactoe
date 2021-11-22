a1a05377a4de58023f9c23345bec0c5d5e644dd4
Mushir-Katote <mushirkatote@gmail.com>
2021-11-22 22:20:40 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get more minimal.</p>
+    </>
+  );
+};
+export default App;
